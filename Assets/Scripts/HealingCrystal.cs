8917a11c9655b36b13bedb3341cf533077522c2d
markphone33-byte <markphone33@gmail.com>
2026-08-20 19:46:02 -0500

    Add Healing Zone and Minimap
    - Remove player's natural healing
    - Add a healing crystal in the starting room which heals the player if they are nearby
    - Add a minimap to the top corner of the player's screen

---
+++ Assets/Scripts/HealingCrystal.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealingCrystal : MonoBehaviour
+{
+    private float nextHealTime;
+    [SerializeField] private float healInterval = 2;
+    [SerializeField] private float healRadius = 2;
+    [SerializeField] private float healPower = 5;
+    private List<Collider> playersToHeal = new List<Collider>();
+
+    void Awake()
+    {
+        nextHealTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (nextHealTime <= Time.time)
+        {
+            foreach (Collider player in playersToHeal)
+            {
+                player.GetComponent<PlayerHealth>()?.Heal(healPower);
+            }
+            nextHealTime = Time.time + healInterval;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            playersToHeal.Add(other);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        playersToHeal.Remove(other);
+    }
+}
